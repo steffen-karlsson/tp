8b2a144a49dd31c347d4eb549645b0afcc731c74
Rune (EC-2) <mail@runetm.dk>
2013-11-24 11:41:51 +0000

    Scripts for cronjob execution

---
+++ executeworker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/admin/tp
+PYTHONPATH=/home/admin/tp/ /usr/bin/python tp/job_controllers/job_executor.py
---
+++ generate_review_jobs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/admin/tp
+PYTHONPATH=/home/admin/tp/ /usr/bin/python tp/job_controllers/create_review_job.py
