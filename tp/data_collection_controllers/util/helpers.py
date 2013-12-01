cc8392e19df4fb4006dae87ab4d1d594dd71b801
skaissuu <ska@issuu.com>
2013-12-01 18:37:17 +0100

    helpers sphinx and pylint

--- tp/data_collection_controllers/util/helpers.py
+++ tp/data_collection_controllers/util/helpers.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. module:: util
+
+"""
 
 from datetime import datetime
 from time import time
 
 
 def to_utc_timstamp(timestamp):
+    """
+
+    Converts local unix time stamp to utc unix time stamp.
+
+    :param timestamp: unix time stamp in local timezone
+    :type timestamp: float
+    :returns: float -- utc unix time stamp
+    """
+
     return float(datetime.utcfromtimestamp(float(timestamp)).strftime('%s'))
 
 
 def now():
-    return int(str(time()).split('.')[0])
+    """
+
+    Converts local unix time stamp to utc unix time stamp.
+
+    :returns: float -- local unix time stamp of now
+    """
+    return float(str(time()).split('.')[0])
