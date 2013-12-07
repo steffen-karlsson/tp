ab5024da2885a0dba9461af7db20bb327d8fab1b
skaissuu <ska@issuu.com>
2013-12-07 13:19:06 +0100

    dummy settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DATABASE_NAME = "database"
+DATABASE_USERNAME = "username"
+DATABASE_PASSWORD = "password"
+DATABASE_HOST = "127.0.0.1"
+DATABASE_PORT = 3306
+
+DEBUG = False
